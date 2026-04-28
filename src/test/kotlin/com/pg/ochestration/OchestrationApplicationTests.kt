8c850b461274d4b2b01dd9728ca67e3f4b7b0ed5
sanseongKo <tkstjd56@naver.com>
2026-04-28 17:13:38 +0900

    refactor(test): 기존 테스트 import 경로 및 주석 정비

--- src/test/kotlin/com/pg/ochestration/OchestrationApplicationTests.kt
+++ src/test/kotlin/com/pg/ochestration/OchestrationApplicationTests.kt
@@ -2,12 +2,28 @@ package com.pg.ochestration
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(
+    properties = [
+        "spring.datasource.url=jdbc:h2:mem:contextdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "spring.sql.init.mode=never",
+        "server.port=0",
+        "gateway.toss-test.base-url=http://localhost",
+        "gateway.toss-test.secret-key=test-secret",
+        "gateway.toss-test.connect-timeout-ms=1000",
+        "gateway.toss-test.read-timeout-ms=1000"
+    ]
+)
 class OchestrationApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
