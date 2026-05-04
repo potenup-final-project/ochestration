2ae884ad28db190f1833ee79c326eb36a63f557a
sanseongKo <tkstjd56@naver.com>
2026-05-04 15:00:02 +0900

    feat(application): 웹훅 dispatch 스케줄러 추가

--- src/main/kotlin/com/pg/ochestration/OchestrationApplication.kt
+++ src/main/kotlin/com/pg/ochestration/OchestrationApplication.kt
@@ -2,8 +2,10 @@ package com.pg.ochestration
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class OchestrationApplication
 
 fun main(args: Array<String>) {
